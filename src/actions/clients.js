6e7adc9656c1c07d1c674a423ec1921de95ecca0
Jim <jimlotruglio@yahoo.com>
2019-02-23 17:17:23 -0700

    set up create action and addClient action

--- src/actions/clients.js
+++ src/actions/clients.js
@@ -1,12 +1,18 @@
 export const addClient = (client)=>{
-  return {
-    type: "ADD_CLIENT",
-    client
+  return (dispatch)=>{
+    return fetch('http://localhost:3001/api/clients',{
+      method: 'POST',
+      body:JSON.stringify({client: client}),
+      headers: {'Content-Type': 'application/json'}
+    }).then(response=> response.json()).then(client=>{
+      return dispatch({type: 'ADD_CLIENT', payload: client})
+    })
   }
 }
 
+
 export function fetchClients(){
-  
+
   return (dispatch)=>{
     dispatch({type: 'LOADING_CLIENTS'})
     return fetch('http://localhost:3001/api/clients').then(response=>{
